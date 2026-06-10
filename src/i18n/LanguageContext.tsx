03f4d30690518b5d8c54839535b581e87df0db9d
Chaos1theory1 <48318079+Chaos1theory1@users.noreply.github.com>
2026-06-10 19:51:17 +0200

    Create LanguageContext.tsx

---
+++ src/i18n/LanguageContext.tsx
@@ -0,0 +1,4 @@
+Stores selected language in localStorage
+Sets document language: <html lang="fr">
+Sets page direction: <html dir="rtl"> for Arabic
+Provides t("translation.key") function to the app
